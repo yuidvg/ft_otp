b3edaa74d664c54b95ab36bc23d6b80c8b2f3f36
Yui Nishimura <nisshi.yui79@gmail.com>
2025-07-01 21:01:23 +0200

    Refactor TOTP and HOTP implementations in Main.hs for clarity and type safety; improve import organization and enhance type annotations.

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,15 @@
 
 module Main (main) where
 
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Base16 as B16
-import qualified Data.ByteString.Char8 as C8
-import qualified Crypto.Hash.SHA1 as SHA1
-import Data.Binary.Get
-import Data.Binary.Put
+import Crypto.Hash.SHA1 qualified as SHA1
+import Data.Binary.Get (getWord32be, runGet)
+import Data.Binary.Put (putWord32be, runPut)
 import Data.Bits
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.ByteString.Base16 qualified as B16
+import Data.ByteString.Char8 qualified as C8
+import Data.ByteString.Lazy qualified as LBS
 import Data.Time.Clock.POSIX
 import Data.Word
 import System.Directory
@@ -25,49 +26,60 @@ keyFileName = "ft_otp.key"
 timeStep :: Word32
 timeStep = 30
 
--- HOTP implementation following RFC 4226
-hotp :: BS.ByteString -> Word32 -> String
-hotp secret counter =
-  let counterBytes = LBS.toStrict $ runPut $ putWord32be counter
-      hmac = SHA1.hmac secret counterBytes
-      offset = fromIntegral (BS.last hmac .&. 0x0f)
-      truncated = runGet getWord32be $ LBS.fromStrict $ BS.drop offset hmac
-      code = (truncated .&. 0x7fffffff) `mod` 1000000
-  in printf "%06d" code
+digit :: Word32
+digit = 6
 
--- TOTP implementation following RFC 6238
-totp :: BS.ByteString -> IO String
-totp secret = do
-  currentTime <- getPOSIXTime
-  let timeCounter = floor currentTime `div` fromIntegral timeStep :: Integer
-  pure $ hotp secret (fromIntegral timeCounter)
+totp :: ByteString -> IO Word32
+totp key =
+  getPOSIXTime >>= \currentTime ->
+    let timeCounter = floor currentTime `div` fromIntegral timeStep :: Integer
+     in pure $ hotp key (fromIntegral timeCounter)
+
+-- HOTP implementation following RFC 4226
+hotp :: ByteString -> Word32 -> Word32
+hotp secret counter = dynamicTruncate $ hmacSha1 secret (LBS.toStrict $ runPut $ putWord32be counter)
+
+dynamicTruncate :: ByteString -> Word32
+dynamicTruncate hs =
+  let sBits = dt hs
+   in sBits `mod` 10 ^ digit
+
+dt :: ByteString -> Word32
+dt hs =
+  let offsetBits = BS.index hs 19 .&. 0x0f
+      offset = fromIntegral offsetBits
+      p = runGet getWord32be $ LBS.fromStrict $ BS.drop offset hs
+   in p .&. 0x7fffffff
+
+hmacSha1 :: ByteString -> ByteString -> ByteString
+hmacSha1 = SHA1.hmac
 
 -- Validate hexadecimal key
-validateHexKey :: String -> Either String BS.ByteString
+validateHexKey :: String -> Either String ByteString
 validateHexKey hexStr =
   if length hexStr < 64
     then Left "key must be 64 hexadecimal characters."
     else case B16.decode (C8.pack hexStr) of
-           Right decoded -> Right decoded
-           Left _ -> Left "key must be valid hexadecimal."
+      Right decoded -> Right decoded
+      Left _ -> Left "key must be valid hexadecimal."
 
 -- Simple encryption/decryption (XOR with fixed key for demonstration)
 -- In production, use proper encryption
-encryptKey :: BS.ByteString -> BS.ByteString
+encryptKey :: ByteString -> ByteString
 encryptKey = BS.map (Data.Bits.xor 0xAA)
 
-decryptKey :: BS.ByteString -> BS.ByteString
+decryptKey :: ByteString -> ByteString
 decryptKey = encryptKey -- XOR is its own inverse
 
 -- Save encrypted key to file
-saveKey :: BS.ByteString -> IO ()
+saveKey :: ByteString -> IO ()
 saveKey key = do
   let encrypted = encryptKey key
   BS.writeFile keyFileName encrypted
   putStrLn $ "Key was successfully saved in " ++ keyFileName ++ "."
 
 -- Load and decrypt key from file
-loadKey :: String -> IO (Either String BS.ByteString)
+loadKey :: String -> IO (Either String ByteString)
 loadKey filename = do
   exists <- doesFileExist filename
   if not exists
@@ -81,36 +93,36 @@ generateTOTP :: String -> IO ()
 generateTOTP filename = do
   result <- loadKey filename
   case result of
+    Right key -> do
+      code <- totp key
+      print code
     Left err -> do
       hPutStrLn stderr $ "./ft_otp: error: " ++ err
       exitFailure
-    Right key -> do
-      code <- totp key
-      putStrLn code
 
 -- Generate key from hex file
-generateFromHex :: String -> IO ()
-generateFromHex filename = do
+generateKey :: String -> IO ()
+generateKey filename = do
   exists <- doesFileExist filename
-  if not exists
+  if exists
     then do
-      hPutStrLn stderr $ "./ft_otp: error: file " ++ filename ++ " does not exist."
-      exitFailure
-    else do
-      content <- readFile filename
-      let hexStr = filter (/= '\n') content
+      content <- BS.readFile filename
+      let hexStr = C8.unpack $ BS.filter (/= 10) content
       case validateHexKey hexStr of
         Left err -> do
           hPutStrLn stderr $ "./ft_otp: error: " ++ err
           exitFailure
         Right key -> saveKey key
+    else do
+      hPutStrLn stderr $ "./ft_otp: error: file " ++ filename ++ " does not exist."
+      exitFailure
 
 -- Main function with argument parsing
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["-g", filename] -> generateFromHex filename
+    ["-g", filename] -> generateKey filename
     ["-k", filename] -> generateTOTP filename
     _ -> do
       hPutStrLn stderr "Usage: ft_otp [-g hex_file | -k key_file]"
